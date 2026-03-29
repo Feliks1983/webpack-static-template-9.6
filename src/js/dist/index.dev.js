f751bac24512120884a5d6ccf37632ef0d7b6afa
Feliks1983 <egaifelya@gmail.com>
2026-03-29 11:04:05 +0900

    message

--- src/js/dist/index.dev.js
+++ src/js/dist/index.dev.js
@@ -8,89 +8,31 @@ require("../scss/style.scss");
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
+function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); if (enumerableOnly) symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; }); keys.push.apply(keys, symbols); } return keys; }
+
+function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; if (i % 2) { ownKeys(source, true).forEach(function (key) { _defineProperty(target, key, source[key]); }); } else if (Object.getOwnPropertyDescriptors) { Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)); } else { ownKeys(source).forEach(function (key) { Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key)); }); } } return target; }
+
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
 var readmore = document.querySelector('.readmore');
 var readmoreText = document.querySelector('.readmore__text');
 var tehnikaReadmore = document.querySelector('.tehnika-readmore');
 var tehnikaBlock = document.querySelectorAll('.tehnika-block');
 var tehnikaReadmoreText = document.querySelector('.tehnika-readmore__text');
-var menuButtonBurger = document.querySelector('.menu__button__burger');
-var menuButtonBurgerMenu = document.querySelector('.menu__button__burger-menu');
-var menuButton = document.querySelector('.menu__button');
-var menuList = document.querySelector('.menu__list');
-var mainSidebar = document.querySelector('.main-sidebar');
-var menuItem = document.querySelector('.menu__item');
-var menuSpans = document.querySelectorAll('.menu__item__span');
-var mainSectionButtons = document.querySelector('.main-section__buttons');
-var btnActive = document.querySelectorAll('.button-active');
-var buttonBurgerBlock = document.querySelectorAll('.button__burger-block');
-var buttonBurgerNone = document.querySelectorAll('.button__burger-none');
-var content = document.querySelector('.content');
 var cardsANone = document.querySelectorAll('.cards__a-none');
+var burgerMenu = document.querySelector('.burger-menu');
+var sidebar = document.querySelector('.sidebar');
+var content = document.querySelector('.content');
+var menuButtonBurger = document.querySelector('.menu__button__burger');
+burgerMenu.addEventListener('click', function () {
+  sidebar.style.width = '320px';
+  sidebar.style.display = 'block';
+  content.style.marginLeft = '320px';
+});
 menuButtonBurger.addEventListener('click', function () {
-  if (menuList.style.display === 'block') {
-    menuList.style.display = 'none';
-  } else {
-    menuList.style.display = 'block';
-  }
-});
-menuItem.addEventListener('click', function (event) {
-  if (event.target.classList.contains('menu__item__span')) {
-    menuSpans.forEach(function (span) {
-      return span.classList.remove('active-span');
-    });
-    event.target.classList.add('active-span');
-  }
-});
-var child = menuItem.children[1];
-var width = window.innerWidth;
-child.addEventListener('click', function () {
-  if (width <= 1440) {
-    var isHidden = window.getComputedStyle(buttonBurgerNone[0]).display === 'none';
-
-    if (!isHidden) {
-      buttonBurgerNone.forEach(function (el) {
-        return el.style.display = 'none';
-      });
-      buttonBurgerBlock.forEach(function (el) {
-        return el.style.display = 'block';
-      });
-    } else {
-      buttonBurgerNone.forEach(function (el) {
-        return el.style.display = 'block';
-      });
-      buttonBurgerBlock.forEach(function (el) {
-        return el.style.display = 'none';
-      });
-    }
-  }
-});
-child.addEventListener('click', function () {
-  if (window.innerWidth >= 1440) {
-    if (content.style.display === 'block') {
-      content.style.display = 'none';
-    } else {
-      content.style.display = 'block';
-    }
-  }
-});
-menuButtonBurgerMenu.addEventListener('click', function () {
-  if (mainSidebar.style.display === 'none' || menuList.style.display === 'block') {
-    mainSidebar.style.display = 'block';
-    menuList.style.display = 'none';
-  } else {
-    mainSidebar.style.display = 'none';
-    menuList.style.display = 'block';
-  }
-});
-mainSectionButtons.addEventListener('click', function (event) {
-  var btnlist = event.target.closest('.button-active');
-
-  if (btnlist) {
-    btnActive.forEach(function (btn) {
-      return btn.classList.remove('active');
-    });
-    btnlist.classList.add('active');
-  }
+  sidebar.style.width = '0px';
+  sidebar.style.display = 'none';
+  content.style.marginLeft = '0px';
 });
 readmore.addEventListener('click', function () {
   var isHidden = cardsANone[0].style.display === 'none';
@@ -106,27 +48,34 @@ tehnikaReadmore.addEventListener('click', function () {
   });
   tehnikaReadmoreText.innerHTML = isHidden ? 'Скрыть' : 'Показать все';
 });
-var swiper1 = new _bundle["default"]('.brends-swiper', {
-  slidesPerView: 2,
+var swiperOptions = {
+  slidesPerView: 'auto',
+  spaceBetween: 16,
+  watchSlidesProgress: true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    dynamicBullets: false
+  }
+};
+var swiper1 = new _bundle["default"]('.brends-swiper', _objectSpread({}, swiperOptions, {
   spaceBetween: 16,
   pagination: {
-    el: '.brends-pagination',
+    el: '.brends-swiper .brends-paginat',
     clickable: true
   }
-});
-var swiper2 = new _bundle["default"]('.tehnika-swiper', {
-  slidesPerView: 2,
+}));
+var swiper2 = new _bundle["default"]('.tehnika-swiper', _objectSpread({}, swiperOptions, {
   spaceBetween: 16,
   pagination: {
-    el: '.tehnika-pagination',
+    el: '.tehnika-swiper .tehnika-paginat',
     clickable: true
   }
-});
-var swiper3 = new _bundle["default"]('.cens-swiper', {
-  slidesPerView: 2,
+}));
+var swiper3 = new _bundle["default"]('.cens-swiper', _objectSpread({}, swiperOptions, {
   spaceBetween: 16,
   pagination: {
-    el: '.cens-pagination',
+    el: '.cens-swiper .cens-paginat',
     clickable: true
   }
-});
+}));
--- src/js/index.js
+++ src/js/index.js
@@ -4,95 +4,25 @@ import '../scss/style.scss'
 
 const readmore = document.querySelector('.readmore')
 const readmoreText = document.querySelector('.readmore__text')
-
 const tehnikaReadmore = document.querySelector('.tehnika-readmore')
 const tehnikaBlock = document.querySelectorAll('.tehnika-block')
-
 const tehnikaReadmoreText = document.querySelector('.tehnika-readmore__text')
-
-const menuButtonBurger = document.querySelector('.menu__button__burger')
-const menuButtonBurgerMenu = document.querySelector(
-  '.menu__button__burger-menu'
-)
-const menuButton = document.querySelector('.menu__button')
-
-const menuList = document.querySelector('.menu__list')
-const mainSidebar = document.querySelector('.main-sidebar')
-const menuItem = document.querySelector('.menu__item')
-const menuSpans = document.querySelectorAll('.menu__item__span')
-
-const mainSectionButtons = document.querySelector('.main-section__buttons')
-const btnActive = document.querySelectorAll('.button-active')
-
-const buttonBurgerBlock = document.querySelectorAll('.button__burger-block')
-const buttonBurgerNone = document.querySelectorAll('.button__burger-none')
-
-const content = document.querySelector('.content')
 const cardsANone = document.querySelectorAll('.cards__a-none')
+const burgerMenu = document.querySelector('.burger-menu')
+const sidebar = document.querySelector('.sidebar')
+const content = document.querySelector('.content')
+const menuButtonBurger = document.querySelector('.menu__button__burger')
 
-menuButtonBurger.addEventListener('click', function () {
-  if (menuList.style.display === 'block') {
-    menuList.style.display = 'none'
-  } else {
-    menuList.style.display = 'block'
-  }
+burgerMenu.addEventListener('click', function () {
+  sidebar.style.width = '320px'
+  sidebar.style.display = 'block'
+  content.style.marginLeft = '320px'
 })
 
-menuItem.addEventListener('click', function (event) {
-  if (event.target.classList.contains('menu__item__span')) {
-    menuSpans.forEach((span) => span.classList.remove('active-span'))
-    event.target.classList.add('active-span')
-  }
-})
-
-const child = menuItem.children[1]
-
-const width = window.innerWidth
-
-child.addEventListener('click', function () {
-  if (width <= 1440) {
-    const isHidden =
-      window.getComputedStyle(buttonBurgerNone[0]).display === 'none'
-
-    if (!isHidden) {
-      buttonBurgerNone.forEach((el) => (el.style.display = 'none'))
-      buttonBurgerBlock.forEach((el) => (el.style.display = 'block'))
-    } else {
-      buttonBurgerNone.forEach((el) => (el.style.display = 'block'))
-      buttonBurgerBlock.forEach((el) => (el.style.display = 'none'))
-    }
-  }
-})
-
-child.addEventListener('click', function () {
-  if (window.innerWidth >= 1440) {
-    if (content.style.display === 'block') {
-      content.style.display = 'none'
-    } else {
-      content.style.display = 'block'
-    }
-  }
-})
-
-menuButtonBurgerMenu.addEventListener('click', function () {
-  if (
-    mainSidebar.style.display === 'none' ||
-    menuList.style.display === 'block'
-  ) {
-    mainSidebar.style.display = 'block'
-    menuList.style.display = 'none'
-  } else {
-    mainSidebar.style.display = 'none'
-    menuList.style.display = 'block'
-  }
-})
-
-mainSectionButtons.addEventListener('click', function (event) {
-  const btnlist = event.target.closest('.button-active')
-  if (btnlist) {
-    btnActive.forEach((btn) => btn.classList.remove('active'))
-    btnlist.classList.add('active')
-  }
+menuButtonBurger.addEventListener('click', function(){
+    sidebar.style.width = '0px'
+    sidebar.style.display = 'none'
+    content.style.marginLeft = '0px'
 })
 
 readmore.addEventListener('click', function () {
@@ -115,32 +45,43 @@ tehnikaReadmore.addEventListener('click', function () {
   tehnikaReadmoreText.innerHTML = isHidden ? 'Скрыть' : 'Показать все'
 })
 
+const swiperOptions = {
+  slidesPerView: 'auto', 
+  spaceBetween: 16,
+  watchSlidesProgress: true,
+  pagination: {
+    el: '.swiper-pagination', 
+    clickable: true,
+    dynamicBullets: false 
+  }
+}
+
 const swiper1 = new Swiper('.brends-swiper', {
-  slidesPerView: 2,
+  ...swiperOptions,
   spaceBetween: 16,
 
   pagination: {
-    el: '.brends-pagination',
+    el: '.brends-swiper .brends-paginat',
     clickable: true
   }
 })
 
 const swiper2 = new Swiper('.tehnika-swiper', {
-  slidesPerView: 2,
+  ...swiperOptions,
   spaceBetween: 16,
 
   pagination: {
-    el: '.tehnika-pagination',
+    el: '.tehnika-swiper .tehnika-paginat',
     clickable: true
   }
 })
 
 const swiper3 = new Swiper('.cens-swiper', {
-  slidesPerView: 2,
+  ...swiperOptions,
   spaceBetween: 16,
 
   pagination: {
-    el: '.cens-pagination',
+    el: '.cens-swiper .cens-paginat',
     clickable: true
   }
 })

d4346e7ceabb9e196f11ceae0afd39e0ee6fc262
Feliks1983 <egaifelya@gmail.com>
2026-04-04 12:17:57 +0900

    'message'

--- src/js/index.js
+++ src/js/index.js
@@ -3,7 +3,9 @@ import 'swiper/css/bundle'
 import '../scss/style.scss'
 
 const readmore = document.querySelector('.readmore')
-const readmoreText = document.querySelector('.readmore__text')
+const readmoreText = document.querySelector('.readmore-text')
+const readmoreImg = document.querySelector('.readmore-img')
+const tehnikaReadmoreImg = document.querySelector('.tehnika-readmore__img')
 const tehnikaReadmore = document.querySelector('.tehnika-readmore')
 const tehnikaBlock = document.querySelectorAll('.tehnika-block')
 const tehnikaReadmoreText = document.querySelector('.tehnika-readmore__text')
@@ -14,17 +16,19 @@ const content = document.querySelector('.content')
 const menuButtonBurger = document.querySelector('.menu__button__burger')
 
 burgerMenu.addEventListener('click', function () {
-  sidebar.style.width = '320px'
-  sidebar.style.display = 'block'
-  content.style.marginLeft = '0px'
+  if (window.innerWidth >= 768) {
+    sidebar.style.width = '320px'
+    sidebar.style.display = 'block'
+    content.style.marginLeft = '0px'
+  }
 })
 
-menuButtonBurger.addEventListener('click', function(){
-    sidebar.style.display = 'none'
+menuButtonBurger.addEventListener('click', function () {
+  sidebar.style.display = 'none'
 })
 
 window.addEventListener('resize', () => {
-  if (window.innerWidth > 1440){
+  if (window.innerWidth > 1440) {
     const sidebar = document.querySelector('.sidebar')
     sidebar.style.removeProperty('display')
   }
@@ -38,6 +42,12 @@ readmore.addEventListener('click', function () {
   })
 
   readmoreText.innerHTML = isHidden ? 'Скрыть' : 'Показать все'
+  if (readmoreText.innerHTML === 'Скрыть') {
+    readmoreImg.style.transform = 'rotate(0deg)'
+  } else {
+    readmoreText.innerHTML = 'Показать все'
+    readmoreImg.style.transform = 'rotate(180deg)'
+  }
 })
 
 tehnikaReadmore.addEventListener('click', function () {
@@ -48,45 +58,40 @@ tehnikaReadmore.addEventListener('click', function () {
   })
 
   tehnikaReadmoreText.innerHTML = isHidden ? 'Скрыть' : 'Показать все'
+  if (tehnikaReadmoreText.innerHTML === 'Скрыть') {
+    tehnikaReadmoreImg.style.transform = 'rotate(0deg)'
+  } else {
+    tehnikaReadmoreText.innerHTML = 'Показать все'
+    tehnikaReadmoreImg.style.transform = 'rotate(180deg)'
+  }
 })
 
-const swiperOptions = {
-  slidesPerView: 'auto', 
-  spaceBetween: 16,
-  watchSlidesProgress: true,
-  pagination: {
-    el: '.swiper-pagination', 
-    clickable: true,
-    dynamicBullets: false 
-  }
-}
-
 const swiper1 = new Swiper('.brends-swiper', {
-  ...swiperOptions,
+  slidesPerView: 'auto',
   spaceBetween: 16,
 
   pagination: {
-    el: '.brends-swiper .brends-paginat',
+    el: '.brends-paginat',
     clickable: true
   }
 })
 
 const swiper2 = new Swiper('.tehnika-swiper', {
-  ...swiperOptions,
+  slidesPerView: 'auto',
   spaceBetween: 16,
 
   pagination: {
-    el: '.tehnika-swiper .tehnika-paginat',
+    el: '.tehnika-paginat',
     clickable: true
   }
 })
 
 const swiper3 = new Swiper('.cens-swiper', {
-  ...swiperOptions,
+  slidesPerView: 'auto',
   spaceBetween: 16,
 
   pagination: {
-    el: '.cens-swiper .cens-paginat',
+    el: '.cens-paginat',
     clickable: true
   }
 })
